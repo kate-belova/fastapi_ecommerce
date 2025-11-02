5d25f80b85070471dc7bd211e8bfa13ecd1556d8
Kate Belova <thebrightestcry@gmail.com>
2025-11-02 14:11:50 +0300

    Logging added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,42 @@
+from uuid import uuid4
+
 import uvicorn
 from fastapi import FastAPI
+from loguru import logger
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from app.routers import routers
 
 app = FastAPI(title='FastAPI Интернет-магазин')
+
+logger.add(
+    'info.log',
+    format='Log: [{extra[log_id]}:{time} - {level} - {message}]',
+    level='INFO',
+    enqueue=True,
+)
+
+
+@app.middleware('http')
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f'Request to {request.url.path} failed')
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f'Request to {request.url.path} failed: {ex}')
+            response = JSONResponse(
+                content={'success': False, 'error': 'Internal server error'},
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+        return response
+
 
 for router in routers:
     app.include_router(router)
